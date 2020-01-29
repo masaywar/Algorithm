aebf2bb060e59168b8cacf010da6fb837dd7270a
masaywar <masaywar@gmail.com>
2020-01-29 17:20:54 +0900

    BackTracking Done

--- BaekJoon/BackTracking.py
+++ BaekJoon/BackTracking.py
@@ -51,51 +51,50 @@ def f4(L, count, M, res):
             f4(L, count + 1, M, res)
             res = res[:-1]
 
-# 9663
-
+#9663
 N = 8
-result = 0
 stack = []
-def N_Queen(x, y, count):
+result = 0
+
+def N_Queen(x, y):
     global result
-    if count == N:
+    if len(stack) == N:
         result += 1
         stack.pop()
         return
     for k in range(N):
         flag = True
-        for postX, postY in stack:
-            if postY == k or abs(postX - x) == abs(postY - k):
+        for px, py in stack:
+            if k == py or abs(x - px) == abs(k - py):
                 flag = False
                 break
-
         if flag:
             stack.append((x, k))
-            N_Queen(x + 1, 0, count + 1)
+            N_Queen(x + 1, 0)
     if stack:
         stack.pop()
 
-# 2580
+#2580
 L = [list(map(int, input().split())) for _ in range(9)]
-zero = [(k, j) for k in range(len(L)) for j in range(len(L)) if L[k][j] == 0] 
+zero =  [(k, j) for k in range(9) for j in range(9) if L[k][j] == 0]
 
 def CheckRow(x, val):
     if val in L[x]:
         return False
-    return True
+    return True    
 
 def CheckCol(y, val):
-    for k in range(0, 9):
+    for k in range(9):
         if L[k][y] == val:
             return False
     return True
 
 def CheckSquare(x, y, val):
-    a = x // 3 * 3
-    b = y // 3 * 3
+    sx = x // 3 * 3
+    sy = y // 3 * 3
     for k in range(3):
         for j in range(3):
-            if L[a + k][b + j] == val:
+            if L[k + sx][j + sy] == val:
                 return False
     return True
 
@@ -106,12 +105,65 @@ def Sudoku(count):
                 print(j, end=" ")
             print()
         sys.exit(0)
-    else:
-        for k in range(1, 10):
-            x, y = zero[count][0], zero[count][1]
-            if CheckRow(x, k) and CheckCol(y, k) and CheckSquare(x, y, k):
-                L[x][y] = k
-                Sudoku(count + 1)
-                L[x][y] = 0
+    for k in range(1, 10):
+        x, y = zero[count][0], zero[count][1]
+        if CheckRow(x, k) and CheckCol(y, k) and CheckSquare(x, y, k):
+            L[x][y] = k
+            Sudoku(count + 1)
+            L[x][y] = 0
 
-Sudoku(0)
+# Sudoku(0)
+
+import math
+
+#14888
+N = int(input())
+L = list(map(int, input().split()))
+plus, minus, mult, divide = map(int, input().split())
+maxVal, minVal = -math.inf, math.inf
+def Op(cur, plus, minus, mult, divide, count):
+    global maxVal, minVal
+    if count == N:
+        maxVal = max(maxVal, cur)
+        minVal = min(minVal, cur)
+        return
+    if plus > 0 :
+        Op(cur + L[count], plus-1, minus, mult, divide, count+1) 
+    if minus > 0 : 
+        Op(cur - L[count], plus, minus-1, mult, divide, count+1) 
+    if mult > 0 :
+        Op(cur * L[count], plus, minus, mult-1, divide, count+1) 
+    if divide > 0 : 
+        Op(cur // L[count] if cur > 0 else - (abs(cur) // L[count]), plus, minus, mult, divide-1, count+1) 
+
+# Op(L[0], plus, minus, mult, divide, 1)
+# print(maxVal)
+# print(minVal)
+
+#14889
+N = int(input())
+L = [list(map(int, input().split())) for _ in range(N)]
+stack = []
+minVal = math.inf
+def StartLink(x):
+    global minVal
+    if len(stack) == N // 2:
+        start, link = 0, 0
+        rest = [x for x in range(N) if x not in stack]
+        for k in stack:
+            for j in stack:
+                if k == j: continue
+                start += L[k][j]
+        for k in rest:
+            for j in rest:
+                if k == j: continue
+                link += L[k][j]
+        minVal = min(minVal, abs(start - link))
+        return
+    for k in range(x, N):
+        stack.append(k)
+        StartLink(k + 1)
+        stack.pop()
+
+StartLink(0)
+print(minVal)
